f839e1f55db62536835092317476ec95dd3f3283
ymd65536 <ymd65536@gmail.com>
2021-11-18 18:56:44 +0900

    Initial Commit

---
+++ Data/ExcelObj.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace ExcelView.Data
+{
+    public class ExcelObj
+    {
+        //private XLWorkbook ImportWb;
+        //private string ImportFileName;
+
+    }
+}
